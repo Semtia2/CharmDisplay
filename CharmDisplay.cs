ab08205c73d380ae49cdb620927e568f5609766d
Semtia <kooshajahani@yahoo.com>
2022-11-10 02:47:17 -0800

    display now disappears and reappears at the appropriate times

--- CharmDisplay.cs
+++ CharmDisplay.cs
@@ -41,12 +41,39 @@ namespace CharmDisplay
             Instance = this;
 
             SetupCanvas();
-            //ModHooks.CharmUpdateHook += UpdateCanvas;
+
+            ModHooks.CharmUpdateHook += UpdateCanvas;
+
+            On.GameManager.FinishedEnteringScene += OnFinishedEnteringScene;
+            On.QuitToMenu.Start += OnQuitToMenu;
+
             On.UIManager.UIClosePauseMenu += OnUnpause;
             On.UIManager.GoToPauseMenu += OnPause;
-            On.UIManager.ContinueGame += OnGameContinue;
+
+            On.InvAnimateUpAndDown.AnimateUp += OnInventoryOpen;
+            //On.InvAnimateUpAndDown.AnimateDown += OnInventoryClose;
+            On.HeroController.AcceptInput += HeroController_AcceptInput;
+
+            On.GameMap.SetupMapMarkers += OnShowMap;
+            //On.GameMap.DisableMarkers += OnHideMap;
+
+            On.HeroController.LeaveScene += HeroController_LeaveScene;
+
+            //On.TransitionPoint. += Teest;
 
             Log("Initialized");
+        }
+
+        private void HeroController_LeaveScene(On.HeroController.orig_LeaveScene orig, HeroController self, GlobalEnums.GatePosition? gate)
+        {
+            orig(self, gate);
+            HideCanvas();
+        }
+
+        private void HeroController_AcceptInput(On.HeroController.orig_AcceptInput orig, HeroController self)
+        {
+            orig(self);
+            ShowCanvas();
         }
 
         private void SetupCanvas()
@@ -66,10 +93,22 @@ namespace CharmDisplay
             };
         }
 
+        private void OnFinishedEnteringScene(On.GameManager.orig_FinishedEnteringScene orig, GameManager self)
+        {
+            orig(self);
+            ShowCanvas();
+        }
+
+        private IEnumerator OnQuitToMenu(On.QuitToMenu.orig_Start orig, QuitToMenu self)
+        {
+            HideCanvas();
+            return orig(self);
+        }
+
         private void OnUnpause(On.UIManager.orig_UIClosePauseMenu origUIClosePauseMenu, UIManager self)
         {
             origUIClosePauseMenu(self);
-            UpdateCanvas();
+            charmPanel.Visibility = Visibility.Visible;
         }
 
         private IEnumerator OnPause(On.UIManager.orig_GoToPauseMenu orig, UIManager uiManager)
@@ -79,15 +118,35 @@ namespace CharmDisplay
             HideCanvas();
         }
 
-        private void OnGameContinue(On.UIManager.orig_ContinueGame orig, UIManager self)
+        private void OnInventoryOpen(On.InvAnimateUpAndDown.orig_AnimateUp orig, InvAnimateUpAndDown self)
         {
             orig(self);
-
-            // UpdateCanvas(); // Causes save file to not load since the equippedCharms doesn't exist yet
+            HideCanvas();
         }
 
-        private void UpdateCanvas()
+        private void OnInventoryClose(On.InvAnimateUpAndDown.orig_AnimateDown orig, InvAnimateUpAndDown self)
         {
+            orig(self);
+            ShowCanvas();
+        }
+
+        private void OnShowMap(On.GameMap.orig_SetupMapMarkers orig, GameMap self)
+        {
+            orig(self);
+            HideCanvas();
+        }
+
+        private void OnHideMap(On.GameMap.orig_DisableMarkers orig, GameMap self)
+        {
+            orig(self);
+            ShowCanvas();
+        }
+
+        private void UpdateCanvas(PlayerData data, HeroController hero)
+        {
+            if (CharmIconList.Instance == null)
+                return;
+
             charmPanel.Children.Clear();
             equippedCharms = PlayerData.instance.equippedCharms;
 
@@ -103,9 +162,14 @@ namespace CharmDisplay
 
         private void HideCanvas()
         {
-            charmPanel.Children.Clear();
+            charmPanel.Visibility = Visibility.Collapsed;
         }
 
-        public override string GetVersion() => "1.1.20";
+        private void ShowCanvas()
+        {
+            charmPanel.Visibility = Visibility.Visible;
+        }
+
+        public override string GetVersion() => "1.1.25";
     }
 }
